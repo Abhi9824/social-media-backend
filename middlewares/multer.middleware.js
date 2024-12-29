aa0e5628c7c44571d3b1db8ab1b3366a62b223cf
Abhi9824 <abhijitchanda2000@gmail.com>
2024-12-30 01:52:24 +0530

    chores:added cloudinary config for post

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,31 +1,17 @@
 const multer = require("multer");
 
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "./public/temp/");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, `${Date.now()}-${file.originalname}`);
-//   },
-// });
-// const upload = multer({ storage });
-
-// module.exports = { upload };
-
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../cloudinary/cloudinary.config"); // Import the cloudinary configuration
+const cloudinary = require("../cloudinary/cloudinary.config");
 
-// Configure Multer to use Cloudinary storage
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "social_media/posts", // Folder in your Cloudinary account
-    resource_type: "auto", // Automatically detect the file type (image/video)
-    allowed_formats: ["jpg", "jpeg", "png", "gif", "mp4", "avi"], // Optional: restrict allowed file types
+    folder: "social_media/posts",
+    resource_type: "auto",
+    allowed_formats: ["jpg", "jpeg", "png", "gif", "mp4", "avi"],
   },
 });
 
-// Create Multer instance with Cloudinary storage
 const upload = multer({ storage });
 
 module.exports = { upload };
